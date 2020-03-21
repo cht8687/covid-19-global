f46c27faa07b8ce83d9733b8d2c68791fe02c0ce
Robert Chang <cht8687@gmail.com>
2020-03-21 14:43:33 +1100

    add AppBar

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,24 @@ import InfoBoard from '../components/infoBoard';
 import Grid from '@material-ui/core/Grid';
 import {useRouter} from 'next/router';
 import {down} from 'styled-breakpoints';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import 'whatwg-fetch';
+
+const AppBarContainer = styled(AppBar)`
+  background-color: green !important;
+`;
+
+const IconButtonContainer = styled(IconButton)`
+  marginright: theme.spacing(2);
+`;
+
+const Title = styled(Typography)`
+  flexgrow: 1;
+`;
 
 const Footer = styled.div`
   text-align: center;
@@ -29,6 +46,14 @@ export default function Index() {
 
   return (
     <>
+      <AppBarContainer position="static">
+        <Toolbar>
+          <IconButtonContainer edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButtonContainer>
+          <Title variant="h6">COVID-19 Live Tracker</Title>
+        </Toolbar>
+      </AppBarContainer>
       <Grid container spacing={2}>
         <Grid item xs={12} lg={6}>
           <Map />
