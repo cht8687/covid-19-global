bef210676e5802e7308cf0f077bf4ecf66201149
Robert Chang <cht8687@gmail.com>
2020-04-05 15:27:58 +1000

    update

--- components/charts/country/pieWithLine/options.js
+++ components/charts/country/pieWithLine/options.js
@@ -39,14 +39,6 @@ export const options = ({source}) => ({
       },
     },
   },
-  visualMap: {
-    show: false,
-    min: 0,
-    max: max,
-    inRange: {
-      symbolSize: [6, 60],
-    },
-  },
   yAxis: {
     gridIndex: 0,
     axisLabel: {
@@ -56,20 +48,6 @@ export const options = ({source}) => ({
     },
   },
   grid: {top: '55%', y: 50, y2: 90},
-  dataZoom: [
-    {
-      show: true,
-      xAxisIndex: [0],
-      start: 60,
-      end: 100,
-    },
-    {
-      type: 'inside',
-      xAxisIndex: [0],
-      start: 60,
-      end: 100,
-    },
-  ],
   series: [
     {
       type: 'line',
